2af00b168a03fe244328decb6880a2a3f6b8af7e
jacob <j.scase@ncl.ac.uk>
2021-12-04 11:49:26 +0000

    Add note models

---
+++ lib/models/Note.dart
@@ -0,0 +1,35 @@
+
+class Note{
+
+  late String userId;
+  late String text;
+  late String imgId;
+  late List<String> tags;
+  late String title;
+  Note({
+    required this.userId, required this.text, required this.tags, required this.title
+    });
+
+
+  Note.fromJson(Map<String, dynamic> json){
+    userId = json['_id'];
+    text = json['userName'];
+    title = json['title'];
+    if(json['imgId'] != null){
+      imgId = json['imgId'];
+    }
+    if(json['tags'] != null){
+      tags = <String>[];
+      json['tags'].forEach((v){
+        tags.add(v);
+      });
+    }
+
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Note{userId: $userId, text: $text , imgId: $imgId , title: $title, tags: $tags}';
+  }
+}
---
+++ lib/models/UserNoteList.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'Note.dart';
+
+class UserNoteList{
+  late List<Note> notes;
+
+  UserNoteList({
+    required this.notes
+  });
+
+  UserNoteList.fromJson(Map<String, dynamic> json){
+    List<dynamic> notesListJSON = (json['notes'] as List<dynamic>);
+
+    notes = <Note>[];
+
+    notesListJSON.forEach((element) {
+
+      Map<String, dynamic> noteJsonMapping = (element as Map<String, dynamic>);
+      notes.add(Note.fromJson(noteJsonMapping));
+
+
+    });
+  }
+
+
+}
